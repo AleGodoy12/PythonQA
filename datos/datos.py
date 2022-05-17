c74534aaeb8cfb8018a666084e8018619e63a891
Arbusta <eyleen.godoy@arbusta.net>
2022-05-17 12:39:28 -0300

    python

--- datos/datos.py
+++ datos/datos.py
@@ -1,3 +1,5 @@
+# Para ejecutar codigo, apretamos shift+enter
+
 # Numericos:
 
 # Pueden ser enteros(init) (negativos o positivos) ó flotantes (float), incluyendo el 0
@@ -7,6 +9,11 @@
 Num = 1 #entero o init
 numero = 1.2 #flotante o float
 cadena = "Hola mundo" #cadena o string
+
+#Booleanos
+
+True #= verdadero
+False #= Falso
 
 #Concatenacion de variables string
 texto1 = "Estamos "
