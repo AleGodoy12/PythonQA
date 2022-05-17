64e94d2c8cec23ac241a6d771d330d3a3cb8b9df
Arbusta <eyleen.godoy@arbusta.net>
2022-05-17 12:31:18 -0300

    prueba

---
+++ datos/datos.py
@@ -0,0 +1,16 @@
+# Numericos:
+
+# Pueden ser enteros(init) (negativos o positivos) ó flotantes (float), incluyendo el 0
+
+#Cadenas de Texto (str)
+
+Num = 1 #entero o init
+numero = 1.2 #flotante o float
+cadena = "Hola mundo" #cadena o string
+
+#Concatenacion de variables string
+texto1 = "Estamos "
+texto2 = "aprendiendo "
+texto3 = "python"
+
+print(texto1 + texto2 + texto3)
