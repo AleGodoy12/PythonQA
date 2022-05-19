8ed54edab50d95b02ac5a3d80490383a745804ec
Luana <luana.acosta@arbusta.net>
2022-05-19 15:17:53 -0300

    Primeros ejercicios de Python. Luana

---
+++ ejercicios/LuanaAcosta/ejerciciosArreglos.py
@@ -0,0 +1,8 @@
+#Declara un arreglo de 3 marcas de autos.
+arreglo=["fiat","ferrari","mercedes benz"]
+
+
+
+
+#Modificar el indice numero 1 del arreglo declarado.
+arreglo=["bmw","ferrari","mercedes benz"]
---
+++ ejercicios/LuanaAcosta/ejerciciosTiposDeDatos.py
@@ -0,0 +1,26 @@
+#Declara una variable por cada tipo de dato (numerico, cadena de texto, booleano)
+
+#numero
+numerito=3
+print(type(numerito))
+#string
+cadenita="Cadenita"
+print(type(cadenita))
+#booleano
+boolean=true
+print(type(boolean))
+
+#Ejercicios 2
+#Realiza una suma, una resta y una multiplicacion con dos variables numericas
+numerito1=2
+numerito2=3
+sumaDeNumeritos=numerito1+numerito2
+multiplicacionDeNumeritos=numerito1*numerito2
+print(sumaDeNumeritos,multiplicacionDeNumeritos)
+
+#Ejercicio 3
+#Realiza una concatenacion de dos variables, la primera con un valor "Hola" y la segunda con valor "numero"
+cadenita1="Blue"
+cadenita2="flame"
+laCadenaFinal=cadenita1+cadenita2
+print(laCadenaFinal)
